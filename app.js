5823fa091d491de783f66351c1994b656d3127ee
Anders Tøttrup <5123677@rts-365.dk>
2018-02-02 13:23:18 +0100

    server

--- app.js
+++ app.js
@@ -1,8 +1,33 @@
 const express = require("express");
 const app = express();
 
-app.get('/api/test2', (req, res) => {
-    res.json({ text: "Hello World" });
+const logger = require('morgan');
+app.use(logger('dev'));
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.get('/api/test', (req, res) => {
+    console.log(req.query);
+    res.json({
+        text: "Hello world"
+    });
+});
+
+app.get('/api/test/:id', (req, res) => {
+    console.log(req.params.id);
+    res.json({ param: req.params.id });
+});
+
+app.post('/api/formular', (req, res) => {
+    res.json({
+        besked: "data blev modtaget",
+        fornavn: req.body.fornavn,
+        efternavn: req.body.efternavn
+    });
 });
 
 app.use(express.static('public'));
